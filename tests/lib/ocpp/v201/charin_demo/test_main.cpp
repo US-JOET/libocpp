03fd710f27b036773dead883f2d10c09005cb681
Gianfranco Berardi <54074967+gberardi-pillar@users.noreply.github.com>
2024-06-04 10:43:21 -0500

    Set input and output directories when specified.
    Signed-off-by: Gianfranco Berardi <54074967+gberardi-pillar@users.noreply.github.com>

--- tests/lib/ocpp/v201/charin_demo/test_main.cpp
+++ tests/lib/ocpp/v201/charin_demo/test_main.cpp
@@ -6,42 +6,43 @@
 #include <boost/program_options/variables_map.hpp>
 #include <gtest/gtest.h>
 #include <string>
- 
-int main(int argc, char ** argv) {
+
+int main(int argc, char** argv) {
 
     std::string input_directory;
     boost::optional<std::string> output_directory;
     boost::program_options::options_description desc("Allowed options");
-    desc.add_options() 
+    desc.add_options()
         ("help", "produce help message")
-        ("input-dir", boost::program_options::value<std::string>()->required(), "path to the directory with json files")
-        ("output-dir", boost::program_options::value<std::string>(), "optional path to spit out stuff")
-    ;
+        ("input-dir",
+            boost::program_options::value<std::string>()->required()->notifier(
+                [&input_directory](std::string path) { input_directory = path; }),
+            "path to the directory with json files")(
+        "output-dir", boost::program_options::value<std::string>()->notifier([&output_directory](std::string path) {
+            output_directory = path;
+        }),
+        "optional path to spit out stuff");
 
     boost::program_options::basic_command_line_parser<char> parser(argc, argv);
     boost::program_options::parsed_options parsed = parser.options(desc).allow_unregistered().run();
     boost::program_options::variables_map vm;
     boost::program_options::store(parsed, vm);
 
-    if (vm.count("help"))
-        {
-            std::cout << desc << "\n";
-            return 1;
-        }
+    if (vm.count("help")) {
+        std::cout << desc << "\n";
+        return 1;
+    }
 
     boost::program_options::notify(vm);
-    for (auto option : parsed.options) {
-        std::cout << "HERE IS AN OPTION: " << option.original_tokens.back() << " UNREGISTERED? " << option.unregistered << std::endl;
+
+    if (!output_directory) {
+        // TODO: Make sure this directory exists.
+        output_directory = "/tmp/EVerest/libocpp/";
     }
 
-    if (output_directory) {
-        std::cout << "output_directory is specified as: " << output_directory << std::endl;
-    } else {
-        std::cout << "No output_directory specified. Using default output" << std::endl;
-    }
+    std::cout << "input_directory: " << input_directory << std::endl;
+    std::cout << "output_directory: " << output_directory << std::endl;
 
-    std::cout << "DESC: " << desc << std::endl;
-    
     // testing::InitGoogleTest(&argc, argv);
     // return RUN_ALL_TESTS();
     return 0;
