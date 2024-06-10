51501e9463fa8104913be60d621a27d8c6424d6c
Christoph <367712+folkengine@users.noreply.github.com>
2024-06-10 10:56:18 -0700

    Updated profiles json imports to be relative to project
    Signed-off-by: Christoph <367712+folkengine@users.noreply.github.com>

--- tests/lib/ocpp/v201/smart_charging_test_utils.hpp
+++ tests/lib/ocpp/v201/smart_charging_test_utils.hpp
@@ -1,3 +1,4 @@
+#include "everest/logging.hpp"
 #include "ocpp/v201/ocpp_types.hpp"
 #include "ocpp/v201/utils.hpp"
 #include <filesystem>
@@ -11,11 +12,12 @@
 
 namespace ocpp::v201 {
 
-static const std::string BASE_JSON_PATH = "/tmp/EVerest/libocpp/v201/json/";
+static const std::string BASE_JSON_PATH = std::string(TEST_PROFILES_LOCATION_V201) + "/json";
 
 class SmartChargingTestUtils {
 public:
     static std::vector<ChargingProfile> get_charging_profiles_from_directory(const std::string& path) {
+        EVLOG_debug << "get_charging_profiles_from_directory: " << path;
         std::vector<ChargingProfile> profiles;
         for (const auto& entry : fs::directory_iterator(path)) {
             if (!entry.is_directory()) {
@@ -31,6 +33,7 @@ public:
     }
 
     static ChargingProfile get_charging_profile_from_path(const std::string& path) {
+        EVLOG_debug << "get_charging_profile_from_path: " << path;
         std::ifstream f(path.c_str());
         json data = json::parse(f);
 
@@ -40,7 +43,7 @@ public:
     }
 
     static ChargingProfile get_charging_profile_from_file(const std::string& filename) {
-        const std::string full_path = BASE_JSON_PATH + filename;
+        const std::string full_path = BASE_JSON_PATH + "/" + filename;
 
         return get_charging_profile_from_path(full_path);
     }
@@ -48,7 +51,7 @@ public:
     /// \brief Returns a vector of ChargingProfiles to be used as a baseline for testing core functionality
     /// of generating an EnhancedChargingSchedule.
     static std::vector<ChargingProfile> get_baseline_profile_vector() {
-        return get_charging_profiles_from_directory(BASE_JSON_PATH + "baseline/");
+        return get_charging_profiles_from_directory(BASE_JSON_PATH + "/" + "baseline/");
     }
 
     static std::string to_string(std::vector<ChargingProfile>& profiles) {
