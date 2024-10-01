440040991587219972bcf4c35e0e486d898ca926
Christopher Davis <150722105+christopher-davis-afs@users.noreply.github.com>
2024-10-01 15:51:26 +0000

    smart_charging, charge_point: Implement K12.FR.04
    Take in the charging limit source, and ensure we don't propagate a
    source of CSO in the off chance we receive it.
    
    Signed-off-by: Christopher Davis <150722105+christopher-davis-afs@users.noreply.github.com>

--- include/ocpp/v201/smart_charging.hpp
+++ include/ocpp/v201/smart_charging.hpp
@@ -110,8 +110,8 @@ public:
                                                            std::optional<ChargingRateUnitEnum> charging_rate_unit) = 0;
 
     virtual std::optional<NotifyChargingLimitRequest>
-    handle_external_limits_changed(const std::variant<float, ChargingSchedule>& limit,
-                                   double percentage_delta) const = 0;
+    handle_external_limits_changed(const std::variant<float, ChargingSchedule>& limit, double percentage_delta,
+                                   ChargingLimitSourceEnum source) const = 0;
 };
 
 /// \brief This class handles and maintains incoming ChargingProfiles and contains the logic
@@ -189,8 +189,8 @@ public:
     /// based on \p percentage_delta and builds the notification.
     ///
     std::optional<NotifyChargingLimitRequest>
-    handle_external_limits_changed(const std::variant<float, ChargingSchedule>& limit,
-                                   double percentage_delta) const override;
+    handle_external_limits_changed(const std::variant<float, ChargingSchedule>& limit, double percentage_delta,
+                                   ChargingLimitSourceEnum source) const override;
 
 protected:
     ///
